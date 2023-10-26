223c760b2ef45ff68fed5cfa3c690577473170e9
declan <declanholland91@gmail.com>
2023-10-26 17:33:13 +0700

    created home page and detailed question cards

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import Question from "@/components/forms/Question";
+import React from "react";
 
 const page = () => {
   return (
     <div>
-        <h1>ask-question</h1>
+      <h1>ask-question</h1>
+      <Question />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
